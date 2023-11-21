4286165bcfaad487c0fbca4de469b473b4c1c55b
serhiyraskoley <r.serogka@gmail.com>
2023-11-21 10:10:27 +0200

    add more product to seed.db

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,19 +9,52 @@
 #   end
 Product.delete_all
 #...
-Product.create!(title: 'Design and Build Great Web APIs',
+
+Product.create!(title: "Docker for Rails Developers",
+  descirption:
+  %{<p>
+  <em>Build, Ship, and Run Your Applications Everywhere</em>
+  Docker does for DevOps what Rails did for web development—it gives 
+  you a new set of superpowers. Gone are “works on my machine” woes and 
+  lengthy setup tasks, replaced instead by a simple, consistent, Docker-based 
+  development environment that will have your team up and running in seconds. 
+  Gain hands-on, real-world experience with a tool that’s rapidly becoming 
+  fundamental to software development. Go from zero all the way to production 
+  as Docker transforms the massive leap of deploying your app in the cloud 
+  into a baby step.
+  </p>},
+  image_url: "ridocker.jpg",
+  price: 24.95)
+
+Product.create!(title: "Modern CSS with Tailwind",
+  descirption:
+  %{<p>
+  <em>Flexible Styling Without the Fuss</em>
+  Tailwind CSS is an exciting new CSS framework you to design your site 
+  by composing simple utility classes to create complex effects. 
+  With Tailwind, you can style your text, move your 
+  items on the page, design complex page layouts, and adapt your design 
+  for devices from a phone to a wide-screen monitor. With this book, 
+  you’ll learn how to use the Tailwind for its flexibility and its 
+  consistency, from the smallest detail of your typography to the 
+  entire design of your site.
+  </p>},
+  image_url: "tailwind.jpg",
+  price: 24.95)
+
+Product.create!(title: "Design and Build Great Web APIs",
   descirption:
   %{<p>
   <em>Robust, Reliable, and Resilient</em>
-  APIs are transforming the business world at an increasing pace. Gain
-  the essential skills needed to quickly design, build, and deploy
-  quality web APIs that are robust, reliable, and resilient. Go from
-  initial design through prototyping and implementation to deployment of
-  mission-critical APIs for your organization. Test, secure, and deploy
-  your API with confidence and avoid the “release into production”
-  panic. Tackle just about any API challenge with more than a dozen
-  open-source utilities and common programming patterns you can apply
-  right away.
+  APIs are transforming the business world at an increasing pace. 
+  Gain the essential skills needed to quickly design, build, and 
+  deploy quality web APIs that are robust, reliable, and resilient. 
+  Go from initial design through prototyping and implementation to 
+  deployment of mission-critical APIs for your organization. 
+  Test, secure, and deploy your API with confidence and avoid the 
+  “release into production” panic. Tackle just about any API challenge 
+  with more than a dozen open-source utilities and common programming 
+  patterns you can apply right away.
   </p>},
-  image_url: 'ridocker.jpg',
+  image_url: "maapis.jpg",
   price: 24.95)
