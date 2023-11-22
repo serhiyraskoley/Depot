c0970c82cfd4fa8955ca8c42ed9e9e5191a21a3a
serhiyraskoley <r.serogka@gmail.com>
2023-11-22 09:34:09 +0200

    Added product title validation and tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 ﻿class Product < ApplicationRecord
   validates :title, :description, :image_url, presence: true
-  validates :title, uniqueness: true
+  validates :title, uniqueness: true, length: { minimum: 10, message: 'must be at least 10 characters long' }
   # validates :image_url, allow_blank: true, format: {
   #   with: %r{\.(gif\jpg\png)\z}i,
   #   message: 'must be a URL for GIF, JPG or PNG image.'
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,6 +1,7 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
+  fixtures :products
   test 'product attributes must not be empty' do
     product = Product.new
     assert product.invalid?                   # assert product is not valid
@@ -11,28 +12,23 @@ class ProductTest < ActiveSupport::TestCase
   end
 
   test 'product price must be positive' do
-    product = Product.new(title: 'My Book Title',
-      description: 'yyy',
-      image_url: 'zzz.jpg')
+    product = Product.new(title: 'My Book Title', description: 'yyy', image_url: 'zzz.jpg')
 
       product.price = -1
       assert product.invalid?                 # assert product is not valid
-      assert_equal ['must be greater than or equal to 0.01'],
-        product.errors[:price]                 # assert product has error on price
+      assert_equal ['must be greater than or equal to 0.01'], product.errors[:price]                 # assert product has error on price
+
       product.price = 0
       assert product.invalid?                 # assert product is not valid
-      assert_equal ['must be greater than or equal to 0.01'],
-        product.errors[:price]                # assert product has error on price
+      assert_equal ['must be greater than or equal to 0.01'], product.errors[:price]                # assert product has error on price
+
       product.price = 1
       assert product.valid?                   # assert product is valid
 
   end
 
   def new_product(image_url)
-    Product.new(title: 'My Book Title',
-      description: 'yyy',
-      price: 1,
-      image_url: image_url)
+    Product.new(title: 'My Book Title', description: 'yyy', price: 1, image_url: image_url)
   end
 
   test 'image url' do
@@ -40,13 +36,32 @@ class ProductTest < ActiveSupport::TestCase
     bad = %w{ fred.doc fred.gif/more fred.gif.more }
 
     ok.each do |image_url|
-      assert new_product(image_url).valid?,
-        "#{image_url} must be valid"
+      assert new_product(image_url).valid?, "#{image_url} must be valid"
     end
 
     bad.each do |image_url|
-      assert new_product(image_url).invalid?,
-        "#{image_url} must be invalid"
+      assert new_product(image_url).invalid?, "#{image_url} must be invalid"
     end
+  end
+
+  test 'product is not valid without a unique title' do
+    product = Product.new(title: products(:ruby).title, description: 'yyy', price: 1, image_url: 'fred.gif')
+
+    assert product.invalid?                   # assert product is not valid
+    assert_equal ['has already been taken'], product.errors[:title]  # assert product has error on title
+  end
+
+  test 'product is not valid without a unique title - i18n' do
+    product = Product.new(title: products(:ruby).title, description: 'yyy', price: 1, image_url: 'fred.gif')
+
+    assert product.invalid?                   # assert product is not valid
+    assert_equal [I18n.translate('errors.messages.taken')], product.errors[:title] # compare error message with i18n and # assert product has error on title
+  end
+
+  test 'product title must be at least 10 characters long' do
+    product = Product.new(title: 'fred', description: 'yyy', price: 1, image_url: 'fred.gif')
+
+    assert product.invalid?                   # assert product is not valid
+    assert_equal ['must be at least 10 characters long'], product.errors[:title]                  # assert product has error on title
   end
 end
