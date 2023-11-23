fce9f74378e562b2a090602975c95f7af3bd9733
serhiyraskoley <r.serogka@gmail.com>
2023-11-23 09:12:21 +0200

    Add a date and time to the sidebar. It doesn’t have to update; just show the value at the time the page was displayed.
    Experiment with setting various number_to_currency helper method options, and see the effect on your catalog listing.
    
    Write some functional tests for the product management application using assert_select. The tests will need to be placed into the test/controllers/products_controller_test.rb file.

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,7 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+    @time_now = Time.now
+    @time_now = @time_now.strftime("%y-%m-%d %H:%M:%S")
   end
 end
